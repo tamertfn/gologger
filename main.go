f30969dc1cf5799174adc3b7273c4984b437f2ad
Hasan Tamer TEFON <httefon@havelsan.com.tr>
2025-03-21 08:21:57 +0300

    JSON format in log file fixed

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package logger
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -20,40 +22,49 @@ const (
 
 // Logger represents a logging system.
 type Logger struct {
-	logger *log.Logger
+	logger            *log.Logger
+	DefaultSaveToFile bool
+	DefaultLogPath    string
 }
 
 /*
- */ /*New creates a new instance of Logger with no flags.
+ */ /*New creates a new instance of Logger with no flags, saveToFile set false. You can configure default setted values.
  */ /*Example usage:
  */ /*
 	func main() {
 		log := logger.New()
+		log.DefaultLogPath = "logs/app.json"
 		startTime := time.Now()
 
 		log.Info(
 			"Information message",
 			logger.LogOptions{
-				StartTime: startTime,
-				User:      "Admin",
-				Process:   "MainProcess",
+				StartTime:   startTime,
+				User:        "Admin",
+				Process:     "MainProcess",
+				SaveToFile:  true,
+				LogFilePath: "./log.json",
 			},
 		)
-		log.Warn("Warning message", logger.LogOptions{User: "Guest", Process: "WorkerProcess"})
+		log.Warn("Warning message", logger.LogOptions{Process: "WorkerProcess", User: "Guest", SaveToFile: true})
 		log.Info("Message only")
 	}
 */
 func New() *Logger {
 	return &Logger{
-		logger: log.New(os.Stdout, "", 0),
+		logger:            log.New(os.Stdout, "", 0),
+		DefaultSaveToFile: false,
+		DefaultLogPath:    "./log.json",
 	}
 }
 
 // LogOptions represents optional parameters for logging.
 type LogOptions struct {
-	StartTime time.Time // The start time of the process
-	Process   string    // The name of the process
-	User      string    // The user associated with the log
+	StartTime   time.Time // The start time of the process
+	Process     string    // The name of the process
+	User        string    // The user associated with the log
+	SaveToFile  bool      // Whether to save the log to file (default: false)
+	LogFilePath string    // Custom path for log file (default: "./log.json")
 }
 
 type logToJSON struct {
@@ -72,6 +83,15 @@ type logToJSON struct {
  */ /* @param "options" optional parameters such as StartTime, Process, and User
  */
 func (l *Logger) logMessage(level, message string, options ...LogOptions) {
+	var opts LogOptions
+	if len(options) > 0 {
+		opts = options[0]
+	}
+
+	// Varsayılan değerleri ayarla
+	if opts.LogFilePath == "" {
+		opts.LogFilePath = l.DefaultLogPath
+	}
 
 	var jsoner logToJSON //var for saving logs to file with json format
 
@@ -85,17 +105,14 @@ func (l *Logger) logMessage(level, message string, options ...LogOptions) {
 	s.Suffix = logParts[0]
 	jsoner.Level = level
 
-	var opts LogOptions
-	if len(options) > 0 {
-		opts = options[0]
-	}
-
+	time.Sleep(1 * time.Second)
 	if opts.Process != "" {
 		logParts = append(logParts, opts.Process)
 		s.Suffix = strings.Join(logParts, " | ")
 		jsoner.Process = opts.Process
 	}
 
+	time.Sleep(1 * time.Second)
 	if !opts.StartTime.IsZero() {
 		currentTime := time.Now()
 		duration := fmt.Sprintf("%d ms", currentTime.Sub(opts.StartTime).Milliseconds())
@@ -105,6 +122,7 @@ func (l *Logger) logMessage(level, message string, options ...LogOptions) {
 		jsoner.Duration = duration
 	}
 
+	time.Sleep(1 * time.Second)
 	if opts.User != "" {
 		logParts = append(logParts, opts.User)
 		s.Suffix = strings.Join(logParts, " | ")
@@ -117,12 +135,21 @@ func (l *Logger) logMessage(level, message string, options ...LogOptions) {
 
 	closingTime := time.Now()
 	jsoner.ClosingTime = closingTime.Format("2006-01-02 15:04:05")
-	s.FinalMSG = closingTime.Format("2006-01-02 15:04:05") + " ✓" + strings.Join(logParts, " | ") + "\n"
+
+	if opts.SaveToFile {
+		if err := saveToFile(jsoner, opts.LogFilePath); err != nil {
+			logParts = append(logParts, "Failed to save log")
+			s.FinalMSG = closingTime.Format("2006-01-02 15:04:05") + " x" + strings.Join(logParts, " | ") + "\n"
+
+			time.Sleep(1 * time.Second)
+		} else {
+			s.FinalMSG = closingTime.Format("2006-01-02 15:04:05") + " ✓" + strings.Join(logParts, " | ") + "\n"
+		}
+	} else {
+		s.FinalMSG = closingTime.Format("2006-01-02 15:04:05") + " ✓" + strings.Join(logParts, " | ") + "\n"
+	}
 
 	s.Stop()
-
-	saveToFile(jsoner)
-
 }
 
 /*
@@ -168,19 +195,75 @@ func (l *Logger) Panic(message string, options ...LogOptions) {
 /*
  */ /*Saves logs to file in JSON format
  */ /* @param "jsoner" logToJSON struct
+ */ /* @param "filePath" path to save the log file
  */
-func saveToFile(jsoner logToJSON) error {
-	file, err := os.OpenFile("log.json", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+func saveToFile(jsoner logToJSON, filePath string) error {
+	// Check directory
+	dir := filepath.Dir(filePath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	// Create file and initialize array if it doesn't exist
+	if !fileExists(filePath) {
+		file, err := os.Create(filePath)
+		if err != nil {
+			return err
+		}
+		_, err = file.WriteString("[\n")
+		if err != nil {
+			return err
+		}
+		file.Close()
+	}
+
+	// Open file
+	file, err := os.OpenFile(filePath, os.O_RDWR, 0644)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	jsonData, err := json.Marshal(jsoner)
+	// Get file size
+	stat, err := file.Stat()
 	if err != nil {
 		return err
 	}
 
-	_, err = file.WriteString(string(jsonData) + "\n")
+	// Create JSON data
+	jsonData, err := json.MarshalIndent(jsoner, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	if stat.Size() > 2 { // File has at least "[\n"
+		// Remove last ']' character
+		if err := file.Truncate(stat.Size() - 2); err != nil {
+			return err
+		}
+		// Add comma and newline
+		if _, err := file.Seek(0, io.SeekEnd); err != nil {
+			return err
+		}
+		if _, err := file.WriteString(",\n"); err != nil {
+			return err
+		}
+	}
+
+	// Append JSON data
+	if _, err := file.Seek(0, io.SeekEnd); err != nil {
+		return err
+	}
+	if _, err := file.Write(jsonData); err != nil {
+		return err
+	}
+
+	// Close array
+	_, err = file.WriteString("\n]")
 	return err
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
 }
